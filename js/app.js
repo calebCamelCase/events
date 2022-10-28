b329d517b26a79f177f80a7b62abff02665fc2e6
Caleb Clark <calebclark0626@gmail.com>
2022-10-28 15:24:33 -0500

    added dynamic memership cards

--- js/app.js
+++ js/app.js
@@ -31,17 +31,37 @@
  * 
  * 
  * condition ? do stuff : do something else
+ * 
+ * 
+ * spread operator
+ * 
+ * arr = [...item]
+ * 
+ * 
+ * string template literal
+ * 
+ * var age = 42
+ * 
+ * var message = 'I am ${age} years old' => I am 43 years old 
  */
 
 const submitBtn = document.getElementById('submitBtn');
+const memberCards = document.getElementById('memberCards');
+let members = []
 
-let data = {};
+// let coders = ['james', 'john', 'caleb'];
+// console.log(coders);
+
+// coders = [...coders, 'shy'];
+// console.log(coders);
+
 
 // data.foo = 'hello';
 
-console.log(data);
+// console.log(data);
 
 const subscribeUser =()=> {
+    let data = {};
     const fName = document.getElementById('fName').value;
     const lName = document.getElementById('lName').value;
     const email = document.getElementById('email').value;
@@ -66,8 +86,36 @@ const subscribeUser =()=> {
     data.date = date;
     data.planType = planType;
 
-    console.log(data);
+    members = [...members, data];
+    return members
+    addUser(data);
+}
 
+const addUser =(obj)=> {
+    // let members = []
+    members = [...members, obj];
+    buildCard(members);
+}
+
+console.log(members);
+
+const buildCard =(arr)=> {
+    let card;
+    arr.forEach(item => {
+        card = `
+        <div class="col-md-3">
+                    <div class="card member-card">
+                        <img src="https://via.placeholder.com/50x50" alt="placeholder" class="img-fluid placeholder-image" />
+                        <div class="card-body">
+                            <p id="lastName">${item.lastName}</p>
+                            <p id="firstName">${item.firstName}</p>
+                            <p id="plan">Plan: ${item.planType}</p>
+                        </div>
+                    </div>
+                </div>
+        `
+    });
+        memberCards.innerHTML+= card
 }
 
 const alertUser =()=> alert('Please agree to terms and conditions')
@@ -75,8 +123,10 @@ const alertUser =()=> alert('Please agree to terms and conditions')
 // console.log(submitBtn)
 submitBtn.addEventListener('click', (e)=> {
     e.preventDefault();
-
+    
     const terms = document.getElementById('terms');
-
+    
     terms.checked ? subscribeUser() : alertUser();  
+    
+    console.log(members);
 })
